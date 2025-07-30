5c62c55041a8d5d5fe4980eca125c5973d332c9e
Peawills <115140417+Peawills@users.noreply.github.com>
2025-07-30 11:44:10 +0100

    Fix

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,12 +1,9 @@
-# library_system.py
-
-
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
-    def get_details(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
@@ -15,7 +12,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def get_details(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -24,7 +21,7 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_details(self):
+    def __str__(self):
         return (
             f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
         )
@@ -35,8 +32,12 @@ class Library:
         self.books = []
 
     def add_book(self, book):
-        self.books.append(book)
+        if isinstance(book, Book):
+            self.books.append(book)
+        else:
+            raise TypeError("Only instances of Book or its subclasses can be added.")
 
     def list_books(self):
         for book in self.books:
-            print(book.get_details())
+            print(book)
+        print("Available books:")
